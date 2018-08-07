64c274ebf88f2d818726963541f3a80f0e94c0c2
Kevin Pittevils <kevinpittevils098@hotmail.com>
2018-08-07 19:05:39 +0200

    compliance 1. Added documentation for different branches 2. added TODO to get the tested implementation with parameters

--- scripts/compliance/compliance-client-test.py
+++ scripts/compliance/compliance-client-test.py
@@ -1,17 +1,19 @@
 import subprocess
 
+# TODO: get to test implementation via command param for logfile
+
 experiment_branches = [
-    "draft-12+PR#1389", # Check the working ,
-    "exp-badly-formed-frame",
-    "exp-data-after-fin",
-    "exp-duplicate-packets",
-    "exp-flow-control",
-    "exp-many-quic-in-udp",
-    "exp-out-of-order",
-    "exp-random-bytes",
-    "exp-request-wrong-stream",
-    "exp-start-hs-frame",
-    "exp-stop-sending-cli-uni",
+    "draft-12+PR#1389", # Check if implementation works with a correct quicker;
+    "exp-badly-formed-frame", # Quicker sents a badly formed frame
+    "exp-data-after-fin", # Quicker sents data after fin bit was set on a stream
+    "exp-duplicate-packets", # Quicker sents all packets duplicate
+    "exp-flow-control", # Quicker sets its max_stream_data very low
+    "exp-many-quic-in-udp", # Quicker sents a lot of handshake packets in a single UDP datagram
+    "exp-out-of-order", # Quicker sents packets reversed from the buffer
+    "exp-random-bytes", # Quicker sents random bytes
+    "exp-request-wrong-stream", # Quicker requests a resource with HTTP on a Server unidirectional stream
+    "exp-start-hs-frame", # Quicker starts with a handshake packet instead of initial packet
+    "exp-stop-sending-cli-uni", # Quicker sents a stop sending frame on a client unidirectional stream
 ]
 
 run_quicker_client = "sh ./scripts/run-scripts/client/build_quicker_and_run.sh 127.0.0.1 4433"
