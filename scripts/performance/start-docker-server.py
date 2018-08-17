9afdedf91ad56961f55e093eceb40999f301890a
Kevin Pittevils <kevinpittevils098@hotmail.com>
2018-08-17 23:25:24 +0200

    performance 1. Import os in startscript

--- scripts/performance/start-docker-server.py
+++ scripts/performance/start-docker-server.py
@@ -2,6 +2,7 @@ import subprocess
 import argparse
 import signal
 import sys
+import os
 
 # Add parent directory to path to import general.py
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
